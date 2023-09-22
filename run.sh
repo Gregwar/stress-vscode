de1714c8c8f2b3c7deeef14320305416c6360c4b
Gregwar <g.passault@gmail.com>
2023-09-22 09:38:43 +0200

    Running code

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ -f symfony ]; then
+if [ ! -f symfony ]; then
     wget https://github.com/symfony-cli/symfony-cli/releases/latest/download/symfony-cli_linux_amd64.tar.gz -O sf.tgz
     tar zxvf sf.tgz
 fi
 
-if [ -f composer.phar ]; then
+if [ ! -f composer.phar ]; then
     wget https://getcomposer.org/composer.phar
 fi
 
 php composer.phar install
+code . src/Controller/DefaultController.php templates/default/index.html.twig
 ./symfony serve
 
